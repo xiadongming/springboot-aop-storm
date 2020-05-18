efce642f2f714f3da76c17e525794449832994f3
xiadongming <dongmingxia@163.com>
2020-05-18 18:35:25 +0800

    storm wordcount

---
+++ springboot-aop-storm/src/main/java/com/example/demo/storm/SplitSentence.java
@@ -0,0 +1,48 @@
+package com.example.demo.storm;
+
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseRichBolt;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Tuple;
+import org.apache.storm.tuple.Values;
+
+import java.util.Map;
+
+/***
+ add by xiadongming on 2020/5/18
+ **/
+public class SplitSentence extends BaseRichBolt {
+    private static final long serialVersionUID = 6604009953652729483L;
+    private OutputCollector collector;
+    /**
+     * 对于bolt来说，第一个方法，就是prepare方法
+     * OutputCollector，这个也是Bolt的这个tuple的发射器
+     */
+    @SuppressWarnings("rawtypes")
+    public void prepare(Map conf, TopologyContext context, OutputCollector collector) {
+        this.collector = collector;
+    }
+
+    /**
+     * execute方法
+     * 就是说，每次接收到一条数据后，就会交给这个executor方法来执行
+     */
+    public void execute(Tuple tuple) {
+        String sentence = tuple.getStringByField("sentence");
+        String[] words = sentence.split(" ");
+        for(String word : words) {
+            collector.emit(new Values(word));
+        }
+    }
+
+    /**
+     * 定义发射出去的tuple，每个field的名称
+     */
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+
+        declarer.declare(new Fields("word"));
+    }
+
+}
---
+++ springboot-aop-storm/src/main/java/com/example/demo/storm/WordCountTopology.java
@@ -0,0 +1,79 @@
+package com.example.demo.storm;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.StormSubmitter;
+import org.apache.storm.spout.SpoutOutputCollector;
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.TopologyBuilder;
+import org.apache.storm.topology.base.BaseRichBolt;
+import org.apache.storm.topology.base.BaseRichSpout;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Tuple;
+import org.apache.storm.tuple.Values;
+import org.apache.storm.utils.Utils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 单词计数拓扑
+ * 
+ * 我认识很多java工程师，都是会一些大数据的技术的，不会太精通，没有那么多的时间去研究
+ * storm的课程，我就只是讲到，最基本的开发，就够了，java开发广告计费系统，大量的流量的引入和接入，就是用storm做得
+ * 用storm，主要是用它的成熟的稳定的易于扩容的分布式系统的特性
+ * java工程师，来说，做一些简单的storm开发，掌握到这个程度差不多就够了
+ * 
+ * @author Administrator
+ *
+ */
+public class WordCountTopology {
+
+	public static void main(String[] args) {
+		// 在main方法中，会去将spout和bolts组合起来，构建成一个拓扑
+		TopologyBuilder builder = new TopologyBuilder();
+	
+		// 这里的第一个参数的意思，就是给这个spout设置一个名字
+		// 第二个参数的意思，就是创建一个spout的对象
+		// 第三个参数的意思，就是设置spout的executor有几个
+		builder.setSpout("RandomSentence", new RandomSentenceSpout(), 2);
+		builder.setBolt("SplitSentence", new SplitSentence(), 5)
+				.setNumTasks(10)
+				.shuffleGrouping("RandomSentence");
+		// 这个很重要，就是说，相同的单词，从SplitSentence发射出来时，一定会进入到下游的指定的同一个task中
+		// 只有这样子，才能准确的统计出每个单词的数量
+		// 比如你有个单词，hello，下游task1接收到3个hello，task2接收到2个hello
+		// 5个hello，全都进入一个task
+		builder.setBolt("WordCount", new WordCount(), 10)
+				.setNumTasks(20)
+				.fieldsGrouping("SplitSentence", new Fields("word"));  
+		
+		Config config = new Config();
+	
+		// 说明是在命令行执行，打算提交到storm集群上去
+		if(args != null && args.length > 0) {
+			config.setNumWorkers(3);  
+			try {
+				StormSubmitter.submitTopology(args[0], config, builder.createTopology());  
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else {
+			// 说明是在eclipse里面本地运行
+			config.setMaxTaskParallelism(20);  
+			
+			LocalCluster cluster = new LocalCluster();
+			cluster.submitTopology("WordCountTopology", config, builder.createTopology());  
+			
+			Utils.sleep(60000); 
+			
+			cluster.shutdown();
+		}
+	}
+	
+}
